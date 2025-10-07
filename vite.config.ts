c80e1ca3072c11b46ecf487fe3275cd0c5014189
VeerDubey <124766244+VeerDubey@users.noreply.github.com>
2025-10-07 13:43:47 +0000

    VB

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // ✅ IMPORTANT: fixes blank page issue on Netlify
+
   server: {
     host: "::",
     port: 8080,
   },
+  
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
